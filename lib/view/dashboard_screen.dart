4026353a9ebd539ce7e1777d2b92eece34ddaedc
USER <thakursusmi94@gmail.com>
2025-05-19 10:25:01 +0545

    Add four button in Dashboard Screen

--- lib/view/dashboard_screen.dart
+++ lib/view/dashboard_screen.dart
@@ -28,19 +28,19 @@ class DashboardScreen extends StatelessWidget {
             ),
           ),
 
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 20),
-          //   child: Column(
-          //     children: [
-          //       _buildDashboardButton("Guided Meditaion"),
-          //       _buildDashboardButton("Mood Tracker"),
-          //       _buildDashboardButton("Journaling"),
-          //       _buildDashboardButton("Progress"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                _buildDashboardButton("Guided Meditaion"),
+                _buildDashboardButton("Mood Tracker"),
+                _buildDashboardButton("Journaling"),
+                _buildDashboardButton("Progress"),
               
-          //     ],
-          //   ),
-          // ),
-          // Spacer(),
+              ],
+            ),
+          ),
+          Spacer(),
 
           // Expanded(child: Container()),
           // Container(
@@ -65,34 +65,34 @@ class DashboardScreen extends StatelessWidget {
     );
   }
 
-  // Widget _buildDashboardButton(String text){
-  //   return Container(
-  //     width: double.infinity,
-  //     margin: EdgeInsets.symmetric(vertical: 8),
-  //     child: ElevatedButton(
-  //       style: ElevatedButton.styleFrom(
-  //         padding: EdgeInsets.symmetric(vertical: 16),
-  //         backgroundColor: Colors.purple.shade900,
-  //         shape: RoundedRectangleBorder(
-  //           borderRadius: BorderRadius.circular(12),
-  //         ),
-  //       ),
-  //       onPressed: () {
+  Widget _buildDashboardButton(String text){
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Colors.purple.shade900,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {
 
-  //       }, 
+        }, 
 
-  //       child: Text(
-  //         text,
-  //         style: TextStyle(
-  //           fontSize: 16,
-  //           fontWeight: FontWeight.bold,
-  //           color: Colors.white
-  //         ),
-  //       ),
-  //     ),
-  //   );
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),
+        ),
+      ),
+    );
 
-  // }
+  }
 
 
   // Widget _buildIconColumn(IconData icon, String label, Color color){
