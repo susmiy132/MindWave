ef27dee722e4901d3843e1606afc128040cc9c11
USER <thakursusmi94@gmail.com>
2025-06-01 14:09:56 +0545

    changes app file

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,17 @@
 
 import 'package:flutter/material.dart';
-import 'package:mindwave/View/splash_screen.dart';
+import 'package:mindwave/bottom_navigation/dashboard.dart';
+import 'package:mindwave/theme/theme_data.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashScreen(),);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardBottomScreen(),
+      theme: getApplicationTheme(),
+    );
   }
 }
