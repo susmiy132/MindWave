6b33f976a12e8d2a586ee9eba08878b03dc15b06
USER <thakursusmi94@gmail.com>
2025-06-20 11:59:05 +0545

    added getcurren user usecase

--- lib/features/auth/domain/use_case/get_current_user_usecase.dart
+++ lib/features/auth/domain/use_case/get_current_user_usecase.dart
@@ -1,17 +1,17 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:mindwave/app/use_case/use_case.dart';
-// import 'package:mindwave/core/error/failure.dart';
-// import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
-// import 'package:mindwave/features/auth/domain/repository/user_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mindwave/app/use_case/use_case.dart';
+import 'package:mindwave/core/error/failure.dart';
+import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
+import 'package:mindwave/features/auth/domain/repository/user_repository.dart';
 
-// class GetCurrentUserUsecase implements UseCaseWithoutParams<UserEntity>{
-//   final IUserRepository _userRepository;
+class GetCurrentUserUsecase implements UseCaseWithoutParams<UserEntity>{
+  final IUserRepository _userRepository;
 
-//   GetCurrentUserUsecase({required IUserRepository userRepository})
-//     : _userRepository = userRepository;
+  GetCurrentUserUsecase({required IUserRepository userRepository})
+    : _userRepository = userRepository;
 
 
-//   @override
-//   Future<Either<Failure, UserEntity>> call() {
-//     return _userRepository.getCurrentUser();
-//   }}
+  @override
+  Future<Either<Failure, UserEntity>> call() {
+    return _userRepository.getCurrentUser();
+  }}
