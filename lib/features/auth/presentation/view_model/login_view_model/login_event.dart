ccfa041162ec7437b5ebdaa74b4d3f65accfbb6e
USER <thakursusmi94@gmail.com>
2025-06-20 12:01:35 +0545

    added login event

--- lib/features/auth/presentation/view_model/login_view_model/login_event.dart
+++ lib/features/auth/presentation/view_model/login_view_model/login_event.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// @immutable
-// sealed class LoginEvent {}
+@immutable
+sealed class LoginEvent {}
 
-// class NavigatetoHomeViewEvent extends LoginEvent{
-//   final BuildContext context;
+class NavigatetoHomeViewEvent extends LoginEvent{
+  final BuildContext context;
 
-//   NavigatetoHomeViewEvent({required this.context});
-// }
+  NavigatetoHomeViewEvent({required this.context});
+}
 
-// class LoginWithEmailAndPasswordEvent extends LoginEvent{
-//   final BuildContext context;
-//   final String email;
-//   final String password;
+class LoginWithEmailAndPasswordEvent extends LoginEvent{
+  final BuildContext context;
+  final String email;
+  final String password;
 
-//   LoginWithEmailAndPasswordEvent({
-//     required this.context,
-//     required this.email,
-//     required this.password
-//   });
-// }
+  LoginWithEmailAndPasswordEvent({
+    required this.context,
+    required this.email,
+    required this.password
+  });
+}
