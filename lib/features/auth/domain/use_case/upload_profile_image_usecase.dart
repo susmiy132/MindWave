f5001224f9f03b803f2ecd4a0bd8b545bae00265
USER <thakursusmi94@gmail.com>
2025-06-20 11:55:56 +0545

    added upload image usecase

--- lib/features/auth/domain/use_case/upload_profile_image_usecase.dart
+++ lib/features/auth/domain/use_case/upload_profile_image_usecase.dart
@@ -1,25 +1,25 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:dartz/dartz.dart';
-// import 'package:mindwave/app/use_case/use_case.dart';
-// import 'package:mindwave/core/error/failure.dart';
-// import 'package:mindwave/features/auth/domain/repository/user_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mindwave/app/use_case/use_case.dart';
+import 'package:mindwave/core/error/failure.dart';
+import 'package:mindwave/features/auth/domain/repository/user_repository.dart';
 
-// class UploadImageParams {
-//   final File file;
+class UploadImageParams {
+  final File file;
 
-//   const UploadImageParams({required this.file});
-// }
+  const UploadImageParams({required this.file});
+}
 
-// class UploadImageUsecase 
-//     implements UseCaseWithParams<String, UploadImageParams>{
-//   final IUserRepository _userRepository;
+class UploadImageUsecase 
+    implements UseCaseWithParams<String, UploadImageParams>{
+  final IUserRepository _userRepository;
 
-//   UploadImageUsecase({required IUserRepository userRepository})
-//     : _userRepository = userRepository;
+  UploadImageUsecase({required IUserRepository userRepository})
+    : _userRepository = userRepository;
 
-//   @override
-//   Future<Either<Failure, String>> call(UploadImageParams params) {
-//     return _userRepository.uploadProfilePicture(params.file);
-//   }
-//     } 
+  @override
+  Future<Either<Failure, String>> call(UploadImageParams params) {
+    return _userRepository.uploadProfilePicture(params.file);
+  }
+    } 
