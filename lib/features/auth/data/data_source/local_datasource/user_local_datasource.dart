00afed19c682e287e8126c221ebbfb9c4b47337b
USER <thakursusmi94@gmail.com>
2025-06-20 11:53:00 +0545

    added local datasource

--- lib/features/auth/data/data_source/local_datasource/user_local_datasource.dart
+++ lib/features/auth/data/data_source/local_datasource/user_local_datasource.dart
@@ -1,54 +1,52 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:mindwave/core/network/hive_service.dart';
-// import 'package:mindwave/features/auth/data/data_source/user_data_source.dart';
-// import 'package:mindwave/features/auth/data/model/user_hive_model.dart';
-// import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
+import 'package:mindwave/core/network/hive_service.dart';
+import 'package:mindwave/features/auth/data/data_source/user_data_source.dart';
+import 'package:mindwave/features/auth/data/model/user_hive_model.dart';
+import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
 
-// class UserLocalDatasource implements IUserDataSource{
+class UserLocalDatasource implements IUserDataSource{
 
-//   final HiveService _hiveService;
+  final HiveService _hiveService;
 
-//   UserLocalDatasource({required HiveService HiveService})
-//     : _hiveService =HiveService;
+  UserLocalDatasource({required HiveService HiveService})
+    : _hiveService =HiveService;
 
 
-//   @override
-//   Future<UserEntity> getCurrentUser() {
+  @override
+  Future<UserEntity> getCurrentUser() {
 
    
-//     // TODO: implement getCurrentUser
-//     throw UnimplementedError();
-//   }
+    throw UnimplementedError();
+  }
 
-//   @override
-//   Future<String> loginUser(String email, String password) async {
-//     try{
-//       final user = await _hiveService.loginUser(email, password);
-//       if(user == null) {
-//         throw Exception('Invalid email or password');
-//       }
-//       return user.userId ?? '';
-//     }catch(e){
-//       throw Exception('Login failed: $e');
-//     }
-//   }
+  @override
+  Future<String> loginUser(String email, String password) async {
+    try{
+      final user = await _hiveService.loginUser(email, password);
+      if(user == null) {
+        throw Exception('Invalid email or password');
+      }
+      return user.userId ?? '';
+    }catch(e){
+      throw Exception('Login failed: $e');
+    }
+  }
 
-//   @override
-//   Future<void> registerUser(UserEntity user) async {
-//     try{
-//       //  To model
-//       final userModel = UserHiveModel.fromEntity(user);
-//       await _hiveService.registerUser(userModel);
-//     }catch(e) {
-//       throw Exception('Registraton failed: $e');
-//     }
-//   }
+  @override
+  Future<void> registerUser(UserEntity user) async {
+    try{
+      //  To model
+      final userModel = UserHiveModel.fromEntity(user);
+      await _hiveService.registerUser(userModel);
+    }catch(e) {
+      throw Exception('Registraton failed: $e');
+    }
+  }
 
-//   @override
-//   Future<String> uploadProfilePicture(File file) {
-//     // TODO: implement uploadProfilePicture
-//     throw UnimplementedError();
-//   }
+  @override
+  Future<String> uploadProfilePicture(File file) {
+    throw UnimplementedError();
+  }
 
-// }
+}
