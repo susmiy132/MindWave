fd0b40dc7067c13bec3320e54282be55c5b2523d
USER <thakursusmi94@gmail.com>
2025-06-20 11:48:28 +0545

    Added auth hive model

--- lib/features/auth/data/model/user_hive_model.g.dart
+++ lib/features/auth/data/model/user_hive_model.g.dart
@@ -1,56 +1,56 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'user_hive_model.dart';
+part of 'user_hive_model.dart';
 
-// // **************************************************************************
-// // TypeAdapterGenerator
-// // **************************************************************************
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
 
-// class UserHiveModelAdapter extends TypeAdapter<UserHiveModel> {
-//   @override
-//   final int typeId = 0;
+class UserHiveModelAdapter extends TypeAdapter<UserHiveModel> {
+  @override
+  final int typeId = 0;
 
-//   @override
-//   UserHiveModel read(BinaryReader reader) {
-//     final numOfFields = reader.readByte();
-//     final fields = <int, dynamic>{
-//       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
-//     };
-//     return UserHiveModel(
-//       userId: fields[0] as String?,
-//       fullName: fields[1] as String,
-//       email: fields[2] as String,
-//       phone: fields[3] as String,
-//       profileImage: fields[4] as String?,
-//       password: fields[5] as String,
-//     );
-//   }
+  @override
+  UserHiveModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return UserHiveModel(
+      userId: fields[0] as String?,
+      fullName: fields[1] as String,
+      email: fields[2] as String,
+      phone: fields[3] as String,
+      profileImage: fields[4] as String?,
+      password: fields[5] as String,
+    );
+  }
 
-//   @override
-//   void write(BinaryWriter writer, UserHiveModel obj) {
-//     writer
-//       ..writeByte(6)
-//       ..writeByte(0)
-//       ..write(obj.userId)
-//       ..writeByte(1)
-//       ..write(obj.fullName)
-//       ..writeByte(2)
-//       ..write(obj.email)
-//       ..writeByte(3)
-//       ..write(obj.phone)
-//       ..writeByte(4)
-//       ..write(obj.profileImage)
-//       ..writeByte(5)
-//       ..write(obj.password);
-//   }
+  @override
+  void write(BinaryWriter writer, UserHiveModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.userId)
+      ..writeByte(1)
+      ..write(obj.fullName)
+      ..writeByte(2)
+      ..write(obj.email)
+      ..writeByte(3)
+      ..write(obj.phone)
+      ..writeByte(4)
+      ..write(obj.profileImage)
+      ..writeByte(5)
+      ..write(obj.password);
+  }
 
-//   @override
-//   int get hashCode => typeId.hashCode;
+  @override
+  int get hashCode => typeId.hashCode;
 
-//   @override
-//   bool operator ==(Object other) =>
-//       identical(this, other) ||
-//       other is UserHiveModelAdapter &&
-//           runtimeType == other.runtimeType &&
-//           typeId == other.typeId;
-// }
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserHiveModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
