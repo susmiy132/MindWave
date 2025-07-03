b56d898ddf5b44bdcd6fd0b476e0d9c7651b0ce6
USER <thakursusmi94@gmail.com>
2025-07-03 18:44:49 +0545

    added sharedPreferences token

--- lib/app/shared_pref/token_shared_prefs.dart
+++ lib/app/shared_pref/token_shared_prefs.dart
@@ -1,22 +1,22 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:mindwave/core/error/failure.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mindwave/core/error/failure.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class TokenSharedPrefs {
-//   final SharedPreferences _sharedPreferences;
+class TokenSharedPrefs {
+  final SharedPreferences _sharedPreferences;
 
-//   TokenSharedPrefs ({required SharedPreferences sharedPreferences})
-//     : _sharedPreferences = sharedPreferences;
+  TokenSharedPrefs ({required SharedPreferences sharedPreferences})
+    : _sharedPreferences = sharedPreferences;
 
-//     Future<Either<Failure, void>> saveToken(String token) async {
-//       try {
-//         await _sharedPreferences.setString('token', token);
-//         return Right(null);
-//       }catch(e){
-//         return Left(
-//           SharedPreferencesFailure(message: 'Failed to save token: $e')
-//         );
-//       }
-//     }
+    Future<Either<Failure, void>> saveToken(String token) async {
+      try {
+        await _sharedPreferences.setString('token', token);
+        return Right(null);
+      }catch(e){
+        return Left(
+          SharedPreferencesFailure(message: 'Failed to save token: $e')
+        );
+      }
+    }
     
-// }
+}
