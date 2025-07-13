5cf666ba422d0cc61fb6c3c09bea9fdc7797b84c
USER <thakursusmi94@gmail.com>
2025-07-13 18:26:25 +0545

    added upload image File

--- lib/features/auth/domain/repository/user_repository.dart
+++ lib/features/auth/domain/repository/user_repository.dart
@@ -7,7 +7,7 @@ import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
 // import 'package:mindwave/features/auth/presentation/view_model/login_view_model/login_event.dart';
 
 abstract interface class IUserRepository {
-  Future<Either<Failure, void>> registerUser(UserEntity user);
+  Future<Either<Failure, void>> registerUser(UserEntity userEntity);
 
   Future<Either<Failure, String>> loginUser(
     // LoginEvent params
