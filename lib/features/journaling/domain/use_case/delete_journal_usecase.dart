62df09f740e77bdab65fd1ed24a9e7b96fc92bac
USER <thakursusmi94@gmail.com>
2025-08-05 00:04:10 +0545

    added delete journal usecase

--- lib/features/journaling/domain/use_case/delete_journal_usecase.dart
+++ lib/features/journaling/domain/use_case/delete_journal_usecase.dart
@@ -1,12 +1,12 @@
-// import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
+import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
 
 
-// class DeleteJournalUseCase {
-//   final JournalRepository repository;
+class DeleteJournalUseCase {
+  final JournalRepository repository;
 
-//   DeleteJournalUseCase(this.repository);
+  DeleteJournalUseCase(this.repository);
 
-//   Future<void> call(String journalId) async {
-//     await repository.deleteJournal(journalId);
-//   }
-// }
+  Future<void> call(String journalId) async {
+    await repository.deleteJournal(journalId);
+  }
+}
