8e25bb2083603654c776ac4faf2be5c604d0b817
USER <thakursusmi94@gmail.com>
2025-08-05 00:01:43 +0545

    added journal local repository

--- lib/features/journaling/data/repository/local_repository/journal_local_repository.dart
+++ lib/features/journaling/data/repository/local_repository/journal_local_repository.dart
@@ -1,35 +1,35 @@
-// import 'package:mindwave/features/journaling/data/data_source/local_datasource/local_data_source.dart';
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
-// import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
+import 'package:mindwave/features/journaling/data/data_source/local_datasource/local_data_source.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
 
-// class JournalRepositoryImpl implements JournalRepository {
-//   final JournalLocalDataSource localDataSource;
+class JournalRepositoryImpl implements JournalRepository {
+  final JournalLocalDataSource localDataSource;
 
-//   JournalRepositoryImpl(this.localDataSource);
+  JournalRepositoryImpl(this.localDataSource);
 
-//   @override
-//   Future<void> addJournal(JournalEntity journal) async {
-//     await localDataSource.addJournal(journal);
-//   }
+  @override
+  Future<void> addJournal(JournalEntity journal) async {
+    await localDataSource.addJournal(journal);
+  }
 
-//   @override
-//   Future<void> deleteJournal(String journalId) async {
-//     await localDataSource.deleteJournal(journalId);
-//   }
+  @override
+  Future<void> deleteJournal(String journalId) async {
+    await localDataSource.deleteJournal(journalId);
+  }
 
-//   @override
-//   Future<void> updateJournal(JournalEntity journal) async {
-//     await localDataSource.updateJournal(journal);
-//   }
+  @override
+  Future<void> updateJournal(JournalEntity journal) async {
+    await localDataSource.updateJournal(journal);
+  }
   
-//   @override
-//   Future<void> saveJournal(JournalEntity journal) {
-//     throw UnimplementedError();
-//   }
+  @override
+  Future<void> saveJournal(JournalEntity journal) {
+    throw UnimplementedError();
+  }
   
-//   @override
-//   Future<List<JournalEntity>> getJournals() {
-//     // TODO: implement getJournals
-//     throw UnimplementedError();
-//   }
-// }
+  @override
+  Future<List<JournalEntity>> getJournals() {
+    // TODO: implement getJournals
+    throw UnimplementedError();
+  }
+}
