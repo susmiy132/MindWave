1f7205ce106bfccb79cf975250d661dbbca897dd
USER <thakursusmi94@gmail.com>
2025-08-04 23:56:32 +0545

    added api journal model

--- lib/features/journaling/data/model/journal_api_model.dart
+++ lib/features/journaling/data/model/journal_api_model.dart
@@ -1,77 +1,77 @@
 
 
 
-// import 'package:equatable/equatable.dart';
-// import 'package:json_annotation/json_annotation.dart';
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
 
-// part 'journal_api_model.g.dart'; // ← This semicolon was missing
+part 'journal_api_model.g.dart'; // ← This semicolon was missing
 
-// @JsonSerializable()
-// class JournalApiModel extends Equatable {
-//   @JsonKey(name: '_id')
-//   final String? journalId;
-//   final DateTime? dateTime;
-//   final String? journalTitle;
-//   final String? journalmood;
-//   final String? place;
-//   final String? weather;
-//   final String? journalImage;
-//   final String? journalContent;
+@JsonSerializable()
+class JournalApiModel extends Equatable {
+  @JsonKey(name: '_id')
+  final String? journalId;
+  final DateTime? dateTime;
+  final String? journalTitle;
+  final String? journalmood;
+  final String? place;
+  final String? weather;
+  final String? journalImage;
+  final String? journalContent;
 
-//   const JournalApiModel({
-//     this.journalId,
-//     required this.dateTime,
-//     required this.journalTitle,
-//     required this.journalmood,
-//     required this.place,
-//     required this.weather,
-//     required this.journalImage,
-//     required this.journalContent,
-//   });
+  const JournalApiModel({
+    this.journalId,
+    required this.dateTime,
+    required this.journalTitle,
+    required this.journalmood,
+    required this.place,
+    required this.weather,
+    required this.journalImage,
+    required this.journalContent,
+  });
 
-//   factory JournalApiModel.fromJson(Map<String, dynamic> json) =>
-//       _$JournalApiModelFromJson(json);
+  factory JournalApiModel.fromJson(Map<String, dynamic> json) =>
+      _$JournalApiModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$JournalApiModelToJson(this);
+  Map<String, dynamic> toJson() => _$JournalApiModelToJson(this);
 
-//   // To Entity
-//   JournalEntity toEntity() {
-//     return JournalEntity(
-//       journalId: journalId,
-//       dateTime: dateTime,
-//       journalTitle: journalTitle,
-//       journalmood: journalmood,
-//       place: place,
-//       weather: weather,
-//       journalImage: journalImage,
-//       journalContent: journalContent,
-//     );
-//   }
+  // To Entity
+  JournalEntity toEntity() {
+    return JournalEntity(
+      journalId: journalId,
+      dateTime: dateTime,
+      journalTitle: journalTitle,
+      journalmood: journalmood,
+      place: place,
+      weather: weather,
+      journalImage: journalImage,
+      journalContent: journalContent,
+    );
+  }
 
-//   // From entity
-//   factory JournalApiModel.fromEntity(JournalEntity entity) {
-//     return JournalApiModel(
-//       journalId: entity.journalId,
-//       dateTime: entity.dateTime,
-//       journalTitle: entity.journalTitle,
-//       journalmood: entity.journalmood,
-//       place: entity.place,
-//       weather: entity.weather,
-//       journalImage: entity.journalImage,
-//       journalContent: entity.journalContent,
-//     );
-//   }
+  // From entity
+  factory JournalApiModel.fromEntity(JournalEntity entity) {
+    return JournalApiModel(
+      journalId: entity.journalId,
+      dateTime: entity.dateTime,
+      journalTitle: entity.journalTitle,
+      journalmood: entity.journalmood,
+      place: entity.place,
+      weather: entity.weather,
+      journalImage: entity.journalImage,
+      journalContent: entity.journalContent,
+    );
+  }
 
-//   @override
-//   List<Object?> get props => [
-//         journalId,
-//         dateTime,
-//         journalTitle,
-//         journalmood,
-//         place,
-//         weather,
-//         journalImage,
-//         journalContent,
-//       ];
-// }
+  @override
+  List<Object?> get props => [
+        journalId,
+        dateTime,
+        journalTitle,
+        journalmood,
+        place,
+        weather,
+        journalImage,
+        journalContent,
+      ];
+}
--- lib/features/journaling/data/model/journal_api_model.g.dart
+++ lib/features/journaling/data/model/journal_api_model.g.dart
@@ -1,33 +1,33 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'journal_api_model.dart';
+part of 'journal_api_model.dart';
 
-// // **************************************************************************
-// // JsonSerializableGenerator
-// // **************************************************************************
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
-// JournalApiModel _$JournalApiModelFromJson(Map<String, dynamic> json) =>
-//     JournalApiModel(
-//       journalId: json['_id'] as String?,
-//       dateTime: json['dateTime'] == null
-//           ? null
-//           : DateTime.parse(json['dateTime'] as String),
-//       journalTitle: json['journalTitle'] as String?,
-//       journalmood: json['journalmood'] as String?,
-//       place: json['place'] as String?,
-//       weather: json['weather'] as String?,
-//       journalImage: json['journalImage'] as String?,
-//       journalContent: json['journalContent'] as String?,
-//     );
+JournalApiModel _$JournalApiModelFromJson(Map<String, dynamic> json) =>
+    JournalApiModel(
+      journalId: json['_id'] as String?,
+      dateTime: json['dateTime'] == null
+          ? null
+          : DateTime.parse(json['dateTime'] as String),
+      journalTitle: json['journalTitle'] as String?,
+      journalmood: json['journalmood'] as String?,
+      place: json['place'] as String?,
+      weather: json['weather'] as String?,
+      journalImage: json['journalImage'] as String?,
+      journalContent: json['journalContent'] as String?,
+    );
 
-// Map<String, dynamic> _$JournalApiModelToJson(JournalApiModel instance) =>
-//     <String, dynamic>{
-//       '_id': instance.journalId,
-//       'dateTime': instance.dateTime?.toIso8601String(),
-//       'journalTitle': instance.journalTitle,
-//       'journalmood': instance.journalmood,
-//       'place': instance.place,
-//       'weather': instance.weather,
-//       'journalImage': instance.journalImage,
-//       'journalContent': instance.journalContent,
-//     };
+Map<String, dynamic> _$JournalApiModelToJson(JournalApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.journalId,
+      'dateTime': instance.dateTime?.toIso8601String(),
+      'journalTitle': instance.journalTitle,
+      'journalmood': instance.journalmood,
+      'place': instance.place,
+      'weather': instance.weather,
+      'journalImage': instance.journalImage,
+      'journalContent': instance.journalContent,
+    };
