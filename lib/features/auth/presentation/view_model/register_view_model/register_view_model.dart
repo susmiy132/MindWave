00385588f383fec6650aaa071c949bdd78063f1f
USER <thakursusmi94@gmail.com>
2025-08-04 23:48:42 +0545

    added register view model

--- lib/features/auth/presentation/view_model/register_view_model/register_view_model.dart
+++ lib/features/auth/presentation/view_model/register_view_model/register_view_model.dart
@@ -1,73 +1,3 @@
-// // // import 'package:flutter/material.dart';
-// // // import 'package:flutter_bloc/flutter_bloc.dart';
-// // // import 'package:mindwave/core/common/my_snackbar.dart/my_snackbar.dart';
-// // // import 'package:mindwave/features/auth/domain/use_case/register_usecase.dart';
-// // // import 'package:mindwave/features/auth/domain/use_case/upload_profile_image_usecase.dart';
-// // // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
-// // // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
-
-// // // class RegisterViewModel extends Bloc<RegisterEvent, RegisterState>{
-// // //   final UserRegisterUsecase _registerUsecase;
-// // //   final UploadImageUsecase _uploadImageUsecase;
-
-// // //   RegisterViewModel(
-// // //     this._registerUsecase,
-// // //     this._uploadImageUsecase,
-// // //   ) : super(RegisterState.initial()) {
-// // //     on<RegisterUserEvent>(_onRegisterUser);
-// // //     on<UploadImageEvent>(_onLoadImage);
-// // //   }
-
-// // //   Future<void> _onRegisterUser(
-// // //     RegisterUserEvent event,
-// // //     Emitter<RegisterState> emit,
-// // //   ) async {
-// // //     emit(state.copyWith(isLoading: true));
-
-// // //     final result = await _registerUsecase(
-// // //       RegisterUsecaseParams(
-// // //         fullName: event.fullName, 
-// // //         email: event.email, 
-// // //         phone: event.phone, 
-// // //         profileImage: state.imageName,
-// // //         password: event.password),
-// // //     );
-
-// // //     result.fold(
-// // //       (l) {
-// // //         emit(state.copyWith(isLoading: false, isSuccess: false));
-// // //         showMySnackBar(
-// // //           context: event.context,
-// // //           message: l.message,
-// // //           color: Colors.red,
-// // //         );
-// // //       },
-// // //       (r) {
-// // //         emit(state.copyWith(isLoading: false, isSuccess: true));
-// // //         showMySnackBar(
-// // //           context: event.context,
-// // //           message: "Registration Successful",
-// // //         );
-// // //       },
-// // //     );
-// // //   }
-
-// // //   void _onLoadImage(UploadImageEvent event, Emitter<RegisterState> emit) async {
-// // //     emit(state.copyWith(isLoading: true));
-// // //     final result = await _uploadImageUsecase.call(
-// // //       UploadImageParams(file: event.file),
-// // //     );
-
-// // //     result.fold(
-// // //       (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
-// // //       (r) {
-// // //         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: r));
-// // //       },
-// // //     );
-// // //   }
-// // // }
-
-
 // // import 'package:flutter/material.dart';
 // // import 'package:flutter_bloc/flutter_bloc.dart';
 // // import 'package:mindwave/core/common/my_snackbar.dart/my_snackbar.dart';
@@ -76,7 +6,7 @@
 // // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
 // // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
 
-// // class RegisterViewModel extends Bloc<RegisterEvent, RegisterState> {
+// // class RegisterViewModel extends Bloc<RegisterEvent, RegisterState>{
 // //   final UserRegisterUsecase _registerUsecase;
 // //   final UploadImageUsecase _uploadImageUsecase;
 
@@ -96,35 +26,28 @@
 
 // //     final result = await _registerUsecase(
 // //       RegisterUsecaseParams(
-// //         fullName: event.fullName,
-// //         email: event.email,
-// //         phone: event.phone,
+// //         fullName: event.fullName, 
+// //         email: event.email, 
+// //         phone: event.phone, 
 // //         profileImage: state.imageName,
-// //         password: event.password,
-// //       ),
+// //         password: event.password),
 // //     );
 
 // //     result.fold(
-// //       (failure) {
+// //       (l) {
 // //         emit(state.copyWith(isLoading: false, isSuccess: false));
 // //         showMySnackBar(
 // //           context: event.context,
-// //           message: failure.message,
-// //           color:Colors.red,
+// //           message: l.message,
+// //           color: Colors.red,
 // //         );
 // //       },
-// //       (success) {
+// //       (r) {
 // //         emit(state.copyWith(isLoading: false, isSuccess: true));
 // //         showMySnackBar(
 // //           context: event.context,
 // //           message: "Registration Successful",
-// //           color: Colors.green,
 // //         );
-
-// //         // Navigate to login page after a short delay
-// //         Future.delayed(const Duration(seconds: 1), () {
-// //           Navigator.pushReplacementNamed(event.context, "/login");
-// //         });
 // //       },
 // //     );
 // //   }
@@ -136,20 +59,20 @@
 // //     );
 
 // //     result.fold(
-// //       (failure) => emit(state.copyWith(isLoading: false, isSuccess: false)),
-// //       (imageName) {
-// //         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: imageName));
+// //       (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
+// //       (r) {
+// //         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: r));
 // //       },
 // //     );
 // //   }
 // // }
+
 
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:mindwave/core/common/my_snackbar.dart/my_snackbar.dart';
 // import 'package:mindwave/features/auth/domain/use_case/register_usecase.dart';
 // import 'package:mindwave/features/auth/domain/use_case/upload_profile_image_usecase.dart';
-// import 'package:mindwave/features/auth/presentation/view/login_view.dart';
 // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
 // import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
 
@@ -183,61 +106,138 @@
 
 //     result.fold(
 //       (failure) {
-//         emit(state.copyWith(isLoading: false, isSuccess: false, errorMessage: failure.message));
+//         emit(state.copyWith(isLoading: false, isSuccess: false));
 //         showMySnackBar(
 //           context: event.context,
 //           message: failure.message,
-//           color: Colors.red,
+//           color:Colors.red,
 //         );
 //       },
 //       (success) {
-//         emit(state.copyWith(isLoading: false, isSuccess: true, errorMessage: '',));
+//         emit(state.copyWith(isLoading: false, isSuccess: true));
 //         showMySnackBar(
 //           context: event.context,
 //           message: "Registration Successful",
 //           color: Colors.green,
 //         );
 
-//         // Navigate after short delay
+//         // Navigate to login page after a short delay
 //         Future.delayed(const Duration(seconds: 1), () {
-//           Navigator.pushReplacement(
-//             event.context,
-//             MaterialPageRoute(builder: (_) => LoginViewScreen()),
-//           );
+//           Navigator.pushReplacementNamed(event.context, "/login");
 //         });
 //       },
 //     );
 //   }
 
-//   Future<void> _onLoadImage(
-//     UploadImageEvent event,
-//     Emitter<RegisterState> emit,
-//   ) async {
+//   void _onLoadImage(UploadImageEvent event, Emitter<RegisterState> emit) async {
 //     emit(state.copyWith(isLoading: true));
-
 //     final result = await _uploadImageUsecase.call(
 //       UploadImageParams(file: event.file),
 //     );
 
 //     result.fold(
-//       (failure) {
-//         emit(state.copyWith(isLoading: false, isSuccess: false));
-//         // ⚠️ If context is needed here, pass it from the event
-//         if (event.context != null) {
-//           showMySnackBar(
-//             context: event.context!,
-//             message: failure.message,
-//             color: Colors.red,
-//           );
-//         }
-//       },
+//       (failure) => emit(state.copyWith(isLoading: false, isSuccess: false)),
 //       (imageName) {
-//         emit(state.copyWith(
-//           isLoading: false,
-//           isSuccess: true,
-//           imageName: imageName,
-//         ));
+//         emit(state.copyWith(isLoading: false, isSuccess: true, imageName: imageName));
 //       },
 //     );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mindwave/core/common/my_snackbar.dart/my_snackbar.dart';
+import 'package:mindwave/features/auth/domain/use_case/register_usecase.dart';
+import 'package:mindwave/features/auth/domain/use_case/upload_profile_image_usecase.dart';
+import 'package:mindwave/features/auth/presentation/view/login_view.dart';
+import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
+import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
+
+class RegisterViewModel extends Bloc<RegisterEvent, RegisterState> {
+  final UserRegisterUsecase _registerUsecase;
+  final UploadImageUsecase _uploadImageUsecase;
+
+  RegisterViewModel(
+    this._registerUsecase,
+    this._uploadImageUsecase,
+  ) : super(RegisterState.initial()) {
+    on<RegisterUserEvent>(_onRegisterUser);
+    on<UploadImageEvent>(_onLoadImage);
+  }
+
+  Future<void> _onRegisterUser(
+    RegisterUserEvent event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+
+    final result = await _registerUsecase(
+      RegisterUsecaseParams(
+        fullName: event.fullName,
+        email: event.email,
+        phone: event.phone,
+        profileImage: state.imageName,
+        password: event.password,
+      ),
+    );
+
+    result.fold(
+      (failure) {
+        emit(state.copyWith(isLoading: false, isSuccess: false, errorMessage: failure.message));
+        showMySnackBar(
+          context: event.context,
+          message: failure.message,
+          color: Colors.red,
+        );
+      },
+      (success) {
+        emit(state.copyWith(isLoading: false, isSuccess: true, errorMessage: '',));
+        showMySnackBar(
+          context: event.context,
+          message: "Registration Successful",
+          color: Colors.green,
+        );
+
+        // Navigate after short delay
+        Future.delayed(const Duration(seconds: 1), () {
+          Navigator.pushReplacement(
+            event.context,
+            MaterialPageRoute(builder: (_) => LoginViewScreen()),
+          );
+        });
+      },
+    );
+  }
+
+  Future<void> _onLoadImage(
+    UploadImageEvent event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+
+    final result = await _uploadImageUsecase.call(
+      UploadImageParams(file: event.file),
+    );
+
+    result.fold(
+      (failure) {
+        emit(state.copyWith(isLoading: false, isSuccess: false));
+        // ⚠️ If context is needed here, pass it from the event
+        if (event.context != null) {
+          showMySnackBar(
+            context: event.context!,
+            message: failure.message,
+            color: Colors.red,
+          );
+        }
+      },
+      (imageName) {
+        emit(state.copyWith(
+          isLoading: false,
+          isSuccess: true,
+          imageName: imageName,
+        ));
+      },
+    );
+  }
+}
