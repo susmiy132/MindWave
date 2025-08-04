459f3148415b18c408da85e32cc970976d0709b8
USER <thakursusmi94@gmail.com>
2025-08-05 01:04:02 +0545

    added mood tracker

--- lib/features/moodtracker/data/model/mood_hive_model.dart
+++ lib/features/moodtracker/data/model/mood_hive_model.dart
@@ -1,65 +1,65 @@
 
-// import 'package:equatable/equatable.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:mindwave/app/constant/hive/hive_table_constraint.dart';
-// import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mindwave/app/constant/hive/hive_table_constraint.dart';
+import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
 
-// part 'mood_hive_model.g.dart';
+part 'mood_hive_model.g.dart';
 
-// // dart run build_runner build -d
+// dart run build_runner build -d
 
-// @HiveType(typeId: 0)
-// class MoodHiveModel extends Equatable {
-//   @HiveField(0)
-//   final String? moodId;
-//   @HiveField(1)
-//   final String moodLabel;
-//   @HiveField(2)
-//   final String moodEmoji;
-//   @HiveField(3)
-//   final DateTime moodDateTime;
+@HiveType(typeId: 0)
+class MoodHiveModel extends Equatable {
+  @HiveField(0)
+  final String? moodId;
+  @HiveField(1)
+  final String moodLabel;
+  @HiveField(2)
+  final String moodEmoji;
+  @HiveField(3)
+  final DateTime moodDateTime;
 
-//   const MoodHiveModel({
-//     this.moodId,
-//     required this.moodLabel,
-//     required this.moodEmoji,
-//     required this.moodDateTime,
-//   });
+  const MoodHiveModel({
+    this.moodId,
+    required this.moodLabel,
+    required this.moodEmoji,
+    required this.moodDateTime,
+  });
 
-//   /// ✅ FIXED: Changed from const to factory
-//   factory MoodHiveModel.initial() {
-//     return MoodHiveModel(
-//       moodId: null,
-//       moodLabel: '',
-//       moodEmoji: '',
-//       moodDateTime: DateTime(2000, 1, 1),
-//     );
-//   }
-//   // Convert to domain entity
-//   MoodEntity toEntity() {
-//     return MoodEntity(
-//       moodId: moodId,
-//       moodLabel: moodLabel,
-//       moodEmoji: moodEmoji,
-//       moodDateTime: moodDateTime,
-//     );
-//   }
+  /// ✅ FIXED: Changed from const to factory
+  factory MoodHiveModel.initial() {
+    return MoodHiveModel(
+      moodId: null,
+      moodLabel: '',
+      moodEmoji: '',
+      moodDateTime: DateTime(2000, 1, 1),
+    );
+  }
+  // Convert to domain entity
+  MoodEntity toEntity() {
+    return MoodEntity(
+      moodId: moodId,
+      moodLabel: moodLabel,
+      moodEmoji: moodEmoji,
+      moodDateTime: moodDateTime,
+    );
+  }
 
-//   // Convert from domain entity
-//   factory MoodHiveModel.fromEntity(MoodEntity entity) {
-//     return MoodHiveModel(
-//       moodId: entity.moodId,
-//       moodLabel: entity.moodLabel,
-//       moodEmoji: entity.moodEmoji,
-//       moodDateTime: entity.moodDateTime,
-//     );
-//   }
+  // Convert from domain entity
+  factory MoodHiveModel.fromEntity(MoodEntity entity) {
+    return MoodHiveModel(
+      moodId: entity.moodId,
+      moodLabel: entity.moodLabel,
+      moodEmoji: entity.moodEmoji,
+      moodDateTime: entity.moodDateTime,
+    );
+  }
 
-//   @override
-//   List<Object?> get props => [
-//         moodId,
-//         moodLabel,
-//         moodEmoji,
-//         moodDateTime,
-//       ];
-// }
+  @override
+  List<Object?> get props => [
+        moodId,
+        moodLabel,
+        moodEmoji,
+        moodDateTime,
+      ];
+}
--- lib/features/moodtracker/data/model/mood_hive_model.g.dart
+++ lib/features/moodtracker/data/model/mood_hive_model.g.dart
@@ -1,50 +1,50 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'mood_hive_model.dart';
+part of 'mood_hive_model.dart';
 
-// // **************************************************************************
-// // TypeAdapterGenerator
-// // **************************************************************************
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
 
-// class MoodHiveModelAdapter extends TypeAdapter<MoodHiveModel> {
-//   @override
-//   final int typeId = 0;
+class MoodHiveModelAdapter extends TypeAdapter<MoodHiveModel> {
+  @override
+  final int typeId = 0;
 
-//   @override
-//   MoodHiveModel read(BinaryReader reader) {
-//     final numOfFields = reader.readByte();
-//     final fields = <int, dynamic>{
-//       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
-//     };
-//     return MoodHiveModel(
-//       moodId: fields[0] as String?,
-//       moodLabel: fields[1] as String,
-//       moodEmoji: fields[2] as String,
-//       moodDateTime: fields[3] as DateTime,
-//     );
-//   }
+  @override
+  MoodHiveModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MoodHiveModel(
+      moodId: fields[0] as String?,
+      moodLabel: fields[1] as String,
+      moodEmoji: fields[2] as String,
+      moodDateTime: fields[3] as DateTime,
+    );
+  }
 
-//   @override
-//   void write(BinaryWriter writer, MoodHiveModel obj) {
-//     writer
-//       ..writeByte(4)
-//       ..writeByte(0)
-//       ..write(obj.moodId)
-//       ..writeByte(1)
-//       ..write(obj.moodLabel)
-//       ..writeByte(2)
-//       ..write(obj.moodEmoji)
-//       ..writeByte(3)
-//       ..write(obj.moodDateTime);
-//   }
+  @override
+  void write(BinaryWriter writer, MoodHiveModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.moodId)
+      ..writeByte(1)
+      ..write(obj.moodLabel)
+      ..writeByte(2)
+      ..write(obj.moodEmoji)
+      ..writeByte(3)
+      ..write(obj.moodDateTime);
+  }
 
-//   @override
-//   int get hashCode => typeId.hashCode;
+  @override
+  int get hashCode => typeId.hashCode;
 
-//   @override
-//   bool operator ==(Object other) =>
-//       identical(this, other) ||
-//       other is MoodHiveModelAdapter &&
-//           runtimeType == other.runtimeType &&
-//           typeId == other.typeId;
-// }
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MoodHiveModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
