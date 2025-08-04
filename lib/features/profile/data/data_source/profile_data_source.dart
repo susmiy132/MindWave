36267eb2b661642bfe2af9fbee1efb56a404a320
USER <thakursusmi94@gmail.com>
2025-08-05 00:51:49 +0545

    added data source

--- lib/features/profile/data/data_source/profile_data_source.dart
+++ lib/features/profile/data/data_source/profile_data_source.dart
@@ -1,9 +1,9 @@
-// // lib/features/profile/data/datasource/profile_data_source.dart
+// lib/features/profile/data/datasource/profile_data_source.dart
 
-// import 'package:mindwave/features/profile/domain/entity/profile_entity.dart';
+import 'package:mindwave/features/profile/domain/entity/profile_entity.dart';
 
-// abstract class ProfileDataSource {
-//   Future<ProfileEntity?> getProfile(String userId);
-//   Future<void> saveProfile(ProfileEntity profile);
-//   Future<void> deleteProfile(String userId);
-// }
+abstract class ProfileDataSource {
+  Future<ProfileEntity?> getProfile(String userId);
+  Future<void> saveProfile(ProfileEntity profile);
+  Future<void> deleteProfile(String userId);
+}
