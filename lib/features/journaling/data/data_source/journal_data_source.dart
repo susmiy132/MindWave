118057c2a0699ee1d5e8d0853c8f35a0d1d49fd0
USER <thakursusmi94@gmail.com>
2025-08-04 23:57:05 +0545

    added journal data source

--- lib/features/journaling/data/data_source/journal_data_source.dart
+++ lib/features/journaling/data/data_source/journal_data_source.dart
@@ -1,8 +1,8 @@
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
 
-// abstract interface class IJournalDataSource {
-//   Future<List<JournalEntity>> getAllJournals(String token);
-//   Future<JournalEntity> createJournal(JournalEntity journal, String token);
-//   Future<JournalEntity> updateJournal(JournalEntity journal, String token);
-//   Future<void> deleteJournal(String journalId, String token);
-// }
+abstract interface class IJournalDataSource {
+  Future<List<JournalEntity>> getAllJournals(String token);
+  Future<JournalEntity> createJournal(JournalEntity journal, String token);
+  Future<JournalEntity> updateJournal(JournalEntity journal, String token);
+  Future<void> deleteJournal(String journalId, String token);
+}
