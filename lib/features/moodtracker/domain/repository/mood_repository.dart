161342e88785337d2172c258c7ab8a3211a41d8e
USER <thakursusmi94@gmail.com>
2025-08-05 01:04:36 +0545

    added mood repository

--- lib/features/moodtracker/domain/repository/mood_repository.dart
+++ lib/features/moodtracker/domain/repository/mood_repository.dart
@@ -1,18 +1,18 @@
-// import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
+import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
 
-// abstract class MoodRepository {
-//   /// Save a mood
-//   Future<void> addMood(MoodEntity mood);
+abstract class MoodRepository {
+  /// Save a mood
+  Future<void> addMood(MoodEntity mood);
 
-//   /// Get all saved moods
-//   Future<List<MoodEntity>> getAllMoods();
+  /// Get all saved moods
+  Future<List<MoodEntity>> getAllMoods();
 
-//   /// Delete a mood by its ID
-//   Future<void> deleteMood(String moodId);
+  /// Delete a mood by its ID
+  Future<void> deleteMood(String moodId);
 
-//   /// Update an existing mood
-//   Future<void> updateMood(MoodEntity mood);
+  /// Update an existing mood
+  Future<void> updateMood(MoodEntity mood);
 
-//   /// Get a single mood by its ID (optional)
-//   Future<MoodEntity?> getMoodById(String moodId);
-// }
+  /// Get a single mood by its ID (optional)
+  Future<MoodEntity?> getMoodById(String moodId);
+}
