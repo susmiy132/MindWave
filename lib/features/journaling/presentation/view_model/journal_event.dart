2bffe27158c47bf29e59827f0a155c0b2dd27680
USER <thakursusmi94@gmail.com>
2025-08-05 00:06:39 +0545

    added journal event

--- lib/features/journaling/presentation/view_model/journal_event.dart
+++ lib/features/journaling/presentation/view_model/journal_event.dart
@@ -1,27 +1,6 @@
-// // import 'package:equatable/equatable.dart';
-// // import '../../domain/entity/journal_entity.dart';
-
-// // abstract class JournalEvent extends Equatable {
-// //   const JournalEvent();
-
-// //   @override
-// //   List<Object?> get props => [];
-// // }
-
-// // class SaveJournalEvent extends JournalEvent {
-// //   final JournalEntity journal;
-
-// //   const SaveJournalEvent(this.journal);
-
-// //   @override
-// //   List<Object?> get props => [journal];
-// // }
-
-
 // import 'package:equatable/equatable.dart';
 // import '../../domain/entity/journal_entity.dart';
 
-// /// Base class for all journal events
 // abstract class JournalEvent extends Equatable {
 //   const JournalEvent();
 
@@ -29,7 +8,6 @@
 //   List<Object?> get props => [];
 // }
 
-// /// Event to save a new journal entry
 // class SaveJournalEvent extends JournalEvent {
 //   final JournalEntity journal;
 
@@ -39,27 +17,49 @@
 //   List<Object?> get props => [journal];
 // }
 
-// /// Event to load all journals (optional but recommended)
-// class LoadJournalsEvent extends JournalEvent {
-//   const LoadJournalsEvent();
-// }
 
-// /// Event to delete a journal by ID
-// class DeleteJournalEvent extends JournalEvent {
-//   final String journalId;
+import 'package:equatable/equatable.dart';
+import '../../domain/entity/journal_entity.dart';
 
-//   const DeleteJournalEvent(this.journalId);
+/// Base class for all journal events
+abstract class JournalEvent extends Equatable {
+  const JournalEvent();
 
-//   @override
-//   List<Object?> get props => [journalId];
-// }
+  @override
+  List<Object?> get props => [];
+}
 
-// /// Event to update an existing journal entry
-// class UpdateJournalEvent extends JournalEvent {
-//   final JournalEntity updatedJournal;
+/// Event to save a new journal entry
+class SaveJournalEvent extends JournalEvent {
+  final JournalEntity journal;
 
-//   const UpdateJournalEvent(this.updatedJournal);
+  const SaveJournalEvent(this.journal);
 
-//   @override
-//   List<Object?> get props => [updatedJournal];
-// }
+  @override
+  List<Object?> get props => [journal];
+}
+
+/// Event to load all journals (optional but recommended)
+class LoadJournalsEvent extends JournalEvent {
+  const LoadJournalsEvent();
+}
+
+/// Event to delete a journal by ID
+class DeleteJournalEvent extends JournalEvent {
+  final String journalId;
+
+  const DeleteJournalEvent(this.journalId);
+
+  @override
+  List<Object?> get props => [journalId];
+}
+
+/// Event to update an existing journal entry
+class UpdateJournalEvent extends JournalEvent {
+  final JournalEntity updatedJournal;
+
+  const UpdateJournalEvent(this.updatedJournal);
+
+  @override
+  List<Object?> get props => [updatedJournal];
+}
