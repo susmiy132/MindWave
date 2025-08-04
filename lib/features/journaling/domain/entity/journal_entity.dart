960bab7b28d2b5a5777d0d488d9d19ee52ab6ee4
USER <thakursusmi94@gmail.com>
2025-08-04 23:54:55 +0545

    added journal entity

--- lib/features/journaling/domain/entity/journal_entity.dart
+++ lib/features/journaling/domain/entity/journal_entity.dart
@@ -1,30 +1,30 @@
-// import 'package:equatable/equatable.dart';
-// import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mindwave/features/auth/domain/entity/user_entity.dart';
 
-// class JournalEntity extends Equatable{
-//   final String? journalId;
-//   final DateTime? dateTime;
-//   final String? journalTitle;
-//   final String? journalmood;
-//   final String? place;
-//   final String? weather;
-//   final String? journalImage;
-//   final String? journalContent;
+class JournalEntity extends Equatable{
+  final String? journalId;
+  final DateTime? dateTime;
+  final String? journalTitle;
+  final String? journalmood;
+  final String? place;
+  final String? weather;
+  final String? journalImage;
+  final String? journalContent;
 
-//   const JournalEntity({
-//     this.journalId,
-//     required this.dateTime,
-//     required this.journalTitle,
-//     required this.journalmood,
-//     required this.place,
-//     required this.weather,
-//     required this.journalImage,
-//     required this.journalContent
-//   });
+  const JournalEntity({
+    this.journalId,
+    required this.dateTime,
+    required this.journalTitle,
+    required this.journalmood,
+    required this.place,
+    required this.weather,
+    required this.journalImage,
+    required this.journalContent
+  });
 
-//   @override
-//   // TODO: implement props
-//   List<Object?> get props => [journalId, dateTime, journalTitle, journalmood, place, weather, journalImage, journalContent];
+  @override
+  // TODO: implement props
+  List<Object?> get props => [journalId, dateTime, journalTitle, journalmood, place, weather, journalImage, journalContent];
 
-//   toEntity() {}
-// }
+  toEntity() {}
+}
