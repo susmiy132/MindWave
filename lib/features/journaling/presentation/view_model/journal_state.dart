3757076ae25cdc7c37477cb03cf0869726f009a1
USER <thakursusmi94@gmail.com>
2025-08-05 00:07:07 +0545

    added journal state

--- lib/features/journaling/presentation/view_model/journal_state.dart
+++ lib/features/journaling/presentation/view_model/journal_state.dart
@@ -1,51 +1,51 @@
-// // import 'package:equatable/equatable.dart';
-
-// // abstract class JournalState extends Equatable {
-// //   const JournalState();
-
-// //   @override
-// //   List<Object?> get props => [];
-// // }
-
-// // class JournalInitial extends JournalState {}
-
-// // class JournalSaving extends JournalState {}
-
-// // class JournalSaved extends JournalState {}
-
-// // class JournalError extends JournalState {
-// //   final String message;
-
-// //   const JournalError(this.message);
-
-// //   @override
-// //   List<Object?> get props => [message];
-// // }
-
-
 // import 'package:equatable/equatable.dart';
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
 
 // abstract class JournalState extends Equatable {
 //   const JournalState();
+
 //   @override
 //   List<Object?> get props => [];
 // }
 
 // class JournalInitial extends JournalState {}
 
-// class JournalLoading extends JournalState {}
+// class JournalSaving extends JournalState {}
 
-// class JournalLoaded extends JournalState {
-//   final List<JournalEntity> journals;
-//   const JournalLoaded(this.journals, {required void journal});
-//   @override
-//   List<Object?> get props => [journals];
-// }
+// class JournalSaved extends JournalState {}
 
 // class JournalError extends JournalState {
 //   final String message;
+
 //   const JournalError(this.message);
+
 //   @override
 //   List<Object?> get props => [message];
 // }
+
+
+import 'package:equatable/equatable.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+
+abstract class JournalState extends Equatable {
+  const JournalState();
+  @override
+  List<Object?> get props => [];
+}
+
+class JournalInitial extends JournalState {}
+
+class JournalLoading extends JournalState {}
+
+class JournalLoaded extends JournalState {
+  final List<JournalEntity> journals;
+  const JournalLoaded(this.journals, {required void journal});
+  @override
+  List<Object?> get props => [journals];
+}
+
+class JournalError extends JournalState {
+  final String message;
+  const JournalError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
