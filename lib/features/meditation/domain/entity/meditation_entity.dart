e3b5e5289c13629d3b30c7155e48870f57c65dd2
USER <thakursusmi94@gmail.com>
2025-08-05 00:13:36 +0545

    added meditation entity

--- lib/features/meditation/domain/entity/meditation_entity.dart
+++ lib/features/meditation/domain/entity/meditation_entity.dart
@@ -1,20 +1,20 @@
-// // lib/features/meditation/domain/entity/meditation_entity.dart
+// lib/features/meditation/domain/entity/meditation_entity.dart
 
-// import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
 
-// class MeditationEntity extends Equatable {
-//   final String id;
-//   final String title;
-//   final String description;
-//   final DateTime createdAt;
+class MeditationEntity extends Equatable {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime createdAt;
 
-//   const MeditationEntity({
-//     required this.id,
-//     required this.title,
-//     required this.description,
-//     required this.createdAt,
-//   });
+  const MeditationEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdAt,
+  });
 
-//   @override
-//   List<Object?> get props => [id, title, description, createdAt];
-// }
+  @override
+  List<Object?> get props => [id, title, description, createdAt];
+}
