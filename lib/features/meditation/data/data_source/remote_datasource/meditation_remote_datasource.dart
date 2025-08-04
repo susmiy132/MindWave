ea01c3d72765644659f0abecb5df1a03b9977c95
USER <thakursusmi94@gmail.com>
2025-08-05 00:19:43 +0545

    added meditation remote datasource

--- lib/features/meditation/data/data_source/remote_datasource/meditation_remote_datasource.dart
+++ lib/features/meditation/data/data_source/remote_datasource/meditation_remote_datasource.dart
@@ -1,30 +1,30 @@
-// import 'package:dio/dio.dart';
-// import 'package:mindwave/app/constant/api_endpoints.dart';
-// import 'package:mindwave/features/meditation/data/model/meditation_api_model.dart';
-// import 'package:mindwave/features/meditation/domain/entity/meditation_entity.dart';
+import 'package:dio/dio.dart';
+import 'package:mindwave/app/constant/api_endpoints.dart';
+import 'package:mindwave/features/meditation/data/model/meditation_api_model.dart';
+import 'package:mindwave/features/meditation/domain/entity/meditation_entity.dart';
 
-// import '../meditation_data_source.dart';
+import '../meditation_data_source.dart';
 
-// class MeditationRemoteDataSource implements MeditationDataSource {
-//   final Dio dio;
+class MeditationRemoteDataSource implements MeditationDataSource {
+  final Dio dio;
 
-//   MeditationRemoteDataSource({required this.dio});
+  MeditationRemoteDataSource({required this.dio});
 
-//   @override
-//   Future<List<MeditationEntity>> getAllMeditations() async {
-//     final response = await dio.get(ApiEndpoints.getAllMeditations);
-//     final List data = response.data;
-//     return data.map((json) => MeditationApiModel.fromJson(json).toEntity()).toList();
-//   }
+  @override
+  Future<List<MeditationEntity>> getAllMeditations() async {
+    final response = await dio.get(ApiEndpoints.getAllMeditations);
+    final List data = response.data;
+    return data.map((json) => MeditationApiModel.fromJson(json).toEntity()).toList();
+  }
 
-//   @override
-//   Future<void> createMeditation(MeditationEntity meditation) async {
-//     final meditationModel = MeditationApiModel.fromEntity(meditation);
-//     await dio.post(ApiEndpoints.createMeditation, data: meditationModel.toJson());
-//   }
+  @override
+  Future<void> createMeditation(MeditationEntity meditation) async {
+    final meditationModel = MeditationApiModel.fromEntity(meditation);
+    await dio.post(ApiEndpoints.createMeditation, data: meditationModel.toJson());
+  }
 
-//   @override
-//   Future<void> deleteMeditation(String id) async {
-//     await dio.delete('${ApiEndpoints.deleteMeditation}/$id');
-//   }
-// }
+  @override
+  Future<void> deleteMeditation(String id) async {
+    await dio.delete('${ApiEndpoints.deleteMeditation}/$id');
+  }
+}
