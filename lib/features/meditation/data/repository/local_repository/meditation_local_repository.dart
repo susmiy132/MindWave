16faee86dbd47cd4cd5a2128663bfcb88294d489
USER <thakursusmi94@gmail.com>
2025-08-05 00:17:51 +0545

    added meditation local repository

--- lib/features/meditation/data/repository/local_repository/meditation_local_repository.dart
+++ lib/features/meditation/data/repository/local_repository/meditation_local_repository.dart
@@ -1,27 +1,27 @@
-// // lib/features/meditation/data/repository/meditation_local_repository.dart
+// lib/features/meditation/data/repository/meditation_local_repository.dart
 
-// import 'package:mindwave/features/meditation/data/data_source/meditation_data_source.dart';
+import 'package:mindwave/features/meditation/data/data_source/meditation_data_source.dart';
 
-// import '../../../domain/entity/meditation_entity.dart';
-// import '../../../domain/repository/meditation_repository.dart';
+import '../../../domain/entity/meditation_entity.dart';
+import '../../../domain/repository/meditation_repository.dart';
 
-// class MeditationLocalRepository implements MeditationRepository {
-//   final MeditationDataSource dataSource;
+class MeditationLocalRepository implements MeditationRepository {
+  final MeditationDataSource dataSource;
 
-//   MeditationLocalRepository(this.dataSource);
+  MeditationLocalRepository(this.dataSource);
 
-//   @override
-//   Future<void> createMeditation(MeditationEntity meditation) {
-//     return dataSource.createMeditation(meditation);
-//   }
+  @override
+  Future<void> createMeditation(MeditationEntity meditation) {
+    return dataSource.createMeditation(meditation);
+  }
 
-//   @override
-//   Future<void> deleteMeditation(String id) {
-//     return dataSource.deleteMeditation(id);
-//   }
+  @override
+  Future<void> deleteMeditation(String id) {
+    return dataSource.deleteMeditation(id);
+  }
 
-//   @override
-//   Future<List<MeditationEntity>> getAllMeditations() {
-//     return dataSource.getAllMeditations();
-//   }
-// }
+  @override
+  Future<List<MeditationEntity>> getAllMeditations() {
+    return dataSource.getAllMeditations();
+  }
+}
