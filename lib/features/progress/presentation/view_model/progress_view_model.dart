d20e1f477675824fc7238cd2f6f894dcd10ed3fe
USER <thakursusmi94@gmail.com>
2025-08-05 00:39:16 +0545

    added progress view model

--- lib/features/progress/presentation/view_model/progress_view_model.dart
+++ lib/features/progress/presentation/view_model/progress_view_model.dart
@@ -1,26 +1,26 @@
-// import 'package:flutter/material.dart';
-// import 'package:mindwave/features/progress/domain/use_case/get_all_progress_usecase.dart';
+import 'package:flutter/material.dart';
+import 'package:mindwave/features/progress/domain/use_case/get_all_progress_usecase.dart';
 
-// import 'progress_event.dart';
-// import 'progress_state.dart';
+import 'progress_event.dart';
+import 'progress_state.dart';
 
-// class ProgressViewModel extends ChangeNotifier {
-//   final GetAllProgressUseCase getAllProgressUseCase;
+class ProgressViewModel extends ChangeNotifier {
+  final GetAllProgressUseCase getAllProgressUseCase;
 
-//   ProgressViewModel(this.getAllProgressUseCase);
+  ProgressViewModel(this.getAllProgressUseCase);
 
-//   ProgressState _state = ProgressInitialState();
-//   ProgressState get state => _state;
+  ProgressState _state = ProgressInitialState();
+  ProgressState get state => _state;
 
-//   Future<void> onEvent(ProgressEvent event) async {
-//     if (event is LoadProgressEvent) {
-//       final data = await getAllProgressUseCase();
-//       if (data.isEmpty) {
-//         _state = ProgressEmptyState();
-//       } else {
-//         _state = ProgressLoadedState(data);
-//       }
-//       notifyListeners();
-//     }
-//   }
-// }
+  Future<void> onEvent(ProgressEvent event) async {
+    if (event is LoadProgressEvent) {
+      final data = await getAllProgressUseCase();
+      if (data.isEmpty) {
+        _state = ProgressEmptyState();
+      } else {
+        _state = ProgressLoadedState(data);
+      }
+      notifyListeners();
+    }
+  }
+}
