f9744b05f932456c4c74840119b7ee4e268be1b9
USER <thakursusmi94@gmail.com>
2025-08-05 01:35:07 +0545

    added progress view model test

--- test/features/progress/presentation/view_model/progress_view_model_test.dart
+++ test/features/progress/presentation/view_model/progress_view_model_test.dart
@@ -1,31 +1,49 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mocktail/mocktail.dart';
-// import 'package:mindwave/features/progress/domain/use_case/get_all_progress_usecase.dart';
-// import 'package:mindwave/features/progress/presentation/view_model/progress_event.dart';
-// import 'package:mindwave/features/progress/presentation/view_model/progress_state.dart';
-// import 'package:mindwave/features/progress/presentation/view_model/progress_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:mindwave/features/progress/domain/use_case/get_all_progress_usecase.dart';
+import 'package:mindwave/features/progress/presentation/view_model/progress_event.dart';
+import 'package:mindwave/features/progress/presentation/view_model/progress_state.dart';
+import 'package:mindwave/features/progress/presentation/view_model/progress_view_model.dart';
 
-// // Mock class for GetAllProgressUseCase
-// class MockGetAllProgressUseCase extends Mock implements GetAllProgressUseCase {}
+// Mock class for GetAllProgressUseCase
+class MockGetAllProgressUseCase extends Mock implements GetAllProgressUseCase {}
 
-// void main() {
-//   late MockGetAllProgressUseCase mockUseCase;
-//   late ProgressViewModel viewModel;
+void main() {
+  late MockGetAllProgressUseCase mockUseCase;
+  late ProgressViewModel viewModel;
 
-//   setUp(() {
-//     mockUseCase = MockGetAllProgressUseCase();
-//     viewModel = ProgressViewModel(mockUseCase);
-//   });
+  setUp(() {
+    mockUseCase = MockGetAllProgressUseCase();
+    viewModel = ProgressViewModel(mockUseCase);
+  });
 
-//   test('Initial state is ProgressInitialState', () {
-//     expect(viewModel.state, isA<ProgressInitialState>());
-//   });
+  test('Initial state is ProgressInitialState', () {
+    expect(viewModel.state, isA<ProgressInitialState>());
+  });
 
-//   test('onEvent with LoadProgressEvent emits ProgressEmptyState when data is empty', () async {
-//     // Arrange: make the use case return empty list
-//     when(() => mockUseCase()).thenAnswer((_) async => []);
+  test('onEvent with LoadProgressEvent emits ProgressEmptyState when data is empty', () async {
+    // Arrange: make the use case return empty list
+    when(() => mockUseCase()).thenAnswer((_) async => []);
 
-//     // Listen for notifyListeners call
+    // Listen for notifyListeners call
+    var notified = false;
+    viewModel.addListener(() {
+      notified = true;
+    });
+
+    // Act
+    await viewModel.onEvent(LoadProgressEvent());
+
+    // Assert
+    expect(viewModel.state, isA<ProgressEmptyState>());
+    expect(notified, isTrue);
+  });
+
+//   test('onEvent with LoadProgressEvent emits ProgressLoadedState when data is not empty', () async {
+//     // Arrange: sample data
+//     final sampleData = ['progress1', 'progress2'];
+//     when(() => mockUseCase()).thenAnswer((_) async => sampleData);
+
 //     var notified = false;
 //     viewModel.addListener(() {
 //       notified = true;
@@ -35,28 +53,10 @@
 //     await viewModel.onEvent(LoadProgressEvent());
 
 //     // Assert
-//     expect(viewModel.state, isA<ProgressEmptyState>());
+//     expect(viewModel.state, isA<ProgressLoadedState>());
+//     expect((viewModel.state as ProgressLoadedState).progressData, sampleData);
 //     expect(notified, isTrue);
 //   });
 
-// //   test('onEvent with LoadProgressEvent emits ProgressLoadedState when data is not empty', () async {
-// //     // Arrange: sample data
-// //     final sampleData = ['progress1', 'progress2'];
-// //     when(() => mockUseCase()).thenAnswer((_) async => sampleData);
 
-// //     var notified = false;
-// //     viewModel.addListener(() {
-// //       notified = true;
-// //     });
-
-// //     // Act
-// //     await viewModel.onEvent(LoadProgressEvent());
-
-// //     // Assert
-// //     expect(viewModel.state, isA<ProgressLoadedState>());
-// //     expect((viewModel.state as ProgressLoadedState).progressData, sampleData);
-// //     expect(notified, isTrue);
-// //   });
-
-
-// }
+}
