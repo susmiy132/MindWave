c694d37b6e4ac3648d07eb833a2a2c112bdcf7a1
USER <thakursusmi94@gmail.com>
2025-08-05 00:31:14 +0545

    added progress entity

--- lib/features/progress/domain/entity/progress_entity.dart
+++ lib/features/progress/domain/entity/progress_entity.dart
@@ -1,31 +1,31 @@
-// // lib/features/progress/domain/entity/progress_entity.dart
+// lib/features/progress/domain/entity/progress_entity.dart
 
-// import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
 
-// class ProgressEntity extends Equatable {
-//   final String? progressId; // optional id for DB
-//   final DateTime date;
-//   final int meditationMinutes;
-//   final String moodLabel;
-//   final String moodIconName; // store icon as String name or code point
-//   final bool journaled;
+class ProgressEntity extends Equatable {
+  final String? progressId; // optional id for DB
+  final DateTime date;
+  final int meditationMinutes;
+  final String moodLabel;
+  final String moodIconName; // store icon as String name or code point
+  final bool journaled;
 
-//   const ProgressEntity({
-//     this.progressId,
-//     required this.date,
-//     required this.meditationMinutes,
-//     required this.moodLabel,
-//     required this.moodIconName,
-//     required this.journaled,
-//   });
+  const ProgressEntity({
+    this.progressId,
+    required this.date,
+    required this.meditationMinutes,
+    required this.moodLabel,
+    required this.moodIconName,
+    required this.journaled,
+  });
 
-//   @override
-//   List<Object?> get props => [
-//         progressId,
-//         date,
-//         meditationMinutes,
-//         moodLabel,
-//         moodIconName,
-//         journaled,
-//       ];
-// }
+  @override
+  List<Object?> get props => [
+        progressId,
+        date,
+        meditationMinutes,
+        moodLabel,
+        moodIconName,
+        journaled,
+      ];
+}
