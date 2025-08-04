38b43e6f3b79684dc98c36c51a4f7957201e96ce
USER <thakursusmi94@gmail.com>
2025-08-05 01:10:10 +0545

    added mood tracker state

--- lib/features/moodtracker/presentation/view_model/mood_tracker_state.dart
+++ lib/features/moodtracker/presentation/view_model/mood_tracker_state.dart
@@ -1,21 +1,21 @@
 
 
-// import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
+import 'package:mindwave/features/moodtracker/domain/entity/mood_entity.dart';
 
-// abstract class MoodTrackerState {}
+abstract class MoodTrackerState {}
 
-// class MoodInitialState extends MoodTrackerState {}
+class MoodInitialState extends MoodTrackerState {}
 
-// class MoodSelectionSuccessState extends MoodTrackerState {
-//   final String selectedMoodLabel;
-//   final String selectedMoodEmoji;
-//   final List<MoodEntity> moodHistory;
+class MoodSelectionSuccessState extends MoodTrackerState {
+  final String selectedMoodLabel;
+  final String selectedMoodEmoji;
+  final List<MoodEntity> moodHistory;
 
-//   MoodSelectionSuccessState({
-//     required this.selectedMoodLabel,
-//     required this.selectedMoodEmoji,
-//     required this.moodHistory,
-//   });
-// }
+  MoodSelectionSuccessState({
+    required this.selectedMoodLabel,
+    required this.selectedMoodEmoji,
+    required this.moodHistory,
+  });
+}
 
-// class MoodEmptyHistoryState extends MoodTrackerState {}
+class MoodEmptyHistoryState extends MoodTrackerState {}
