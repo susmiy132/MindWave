ec28016f482a6db7e1393475d8b3e615ea952ddb
USER <thakursusmi94@gmail.com>
2025-08-05 01:19:25 +0545

    added register view test

--- test/features/auth/presentation/view/register_view_test.dart
+++ test/features/auth/presentation/view/register_view_test.dart
@@ -1,38 +1,38 @@
-// // Widget Testing
+// Widget Testing
 
-// import 'package:bloc_test/bloc_test.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mindwave/features/auth/presentation/view/register_view.dart';
-// import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
-// import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
-// import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_view_model.dart';
-// import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mindwave/features/auth/presentation/view/register_view.dart';
+import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_event.dart';
+import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_state.dart';
+import 'package:mindwave/features/auth/presentation/view_model/register_view_model/register_view_model.dart';
+import 'package:mocktail/mocktail.dart';
 
-// class MockRegisterViewModel extends MockBloc<RegisterEvent, RegisterState> implements RegisterViewModel{}
-// void main() {
-//   late MockRegisterViewModel registerViewModel;
+class MockRegisterViewModel extends MockBloc<RegisterEvent, RegisterState> implements RegisterViewModel{}
+void main() {
+  late MockRegisterViewModel registerViewModel;
 
-//   setUp(() {
-//     registerViewModel = MockRegisterViewModel();
-//   });
+  setUp(() {
+    registerViewModel = MockRegisterViewModel();
+  });
 
-//   Widget loadRegisterViewModel() {
-//     return BlocProvider<RegisterViewModel>(
-//       create: (context) => registerViewModel,
-//       child: MaterialApp(home: RegisterView()),
-//     );
-//   }
-//   testWidgets('register view model ...', (tester) async {
+  Widget loadRegisterViewModel() {
+    return BlocProvider<RegisterViewModel>(
+      create: (context) => registerViewModel,
+      child: MaterialApp(home: RegisterView()),
+    );
+  }
+  testWidgets('register view model ...', (tester) async {
 
-//     when(() => registerViewModel.state).thenReturn(RegisterState.initial());
+    when(() => registerViewModel.state).thenReturn(RegisterState.initial());
 
-//     await tester.pumpWidget(loadRegisterViewModel());
-//     await tester.pumpAndSettle();
+    await tester.pumpWidget(loadRegisterViewModel());
+    await tester.pumpAndSettle();
 
-//     // final button by text
-//     final result = find.widgetWithText(ElevatedButton, 'Register');
-//     expect(result, findsOneWidget);
-//   });
-// }
+    // final button by text
+    final result = find.widgetWithText(ElevatedButton, 'Register');
+    expect(result, findsOneWidget);
+  });
+}
