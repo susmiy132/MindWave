11a3689a3e8e909bc02bf651f34e052d2fa295c4
USER <thakursusmi94@gmail.com>
2025-08-05 01:23:40 +0545

    added delete journal usecase testing

--- test/features/journaling/domain/use_case/delete_journal_usecase_test.dart
+++ test/features/journaling/domain/use_case/delete_journal_usecase_test.dart
@@ -1,31 +1,31 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mocktail/mocktail.dart';
-// import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
-// import 'package:mindwave/features/journaling/domain/use_case/delete_journal_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
+import 'package:mindwave/features/journaling/domain/use_case/delete_journal_usecase.dart';
 
-// // Mock class
-// class MockJournalRepository extends Mock implements JournalRepository {}
+// Mock class
+class MockJournalRepository extends Mock implements JournalRepository {}
 
-// void main() {
-//   late MockJournalRepository mockJournalRepository;
-//   late DeleteJournalUseCase deleteJournalUseCase;
+void main() {
+  late MockJournalRepository mockJournalRepository;
+  late DeleteJournalUseCase deleteJournalUseCase;
 
-//   const testJournalId = 'journal123';
+  const testJournalId = 'journal123';
 
-//   setUp(() {
-//     mockJournalRepository = MockJournalRepository();
-//     deleteJournalUseCase = DeleteJournalUseCase(mockJournalRepository);
-//   });
+  setUp(() {
+    mockJournalRepository = MockJournalRepository();
+    deleteJournalUseCase = DeleteJournalUseCase(mockJournalRepository);
+  });
 
-//   test('should call repository to delete journal', () async {
-//     // Arrange
-//     when(() => mockJournalRepository.deleteJournal(any()))
-//         .thenAnswer((_) async {});
+  test('should call repository to delete journal', () async {
+    // Arrange
+    when(() => mockJournalRepository.deleteJournal(any()))
+        .thenAnswer((_) async {});
 
-//     // Act
-//     await deleteJournalUseCase(testJournalId);
+    // Act
+    await deleteJournalUseCase(testJournalId);
 
-//     // Assert
-//     verify(() => mockJournalRepository.deleteJournal(testJournalId)).called(1);
-//   });
-// }
+    // Assert
+    verify(() => mockJournalRepository.deleteJournal(testJournalId)).called(1);
+  });
+}
