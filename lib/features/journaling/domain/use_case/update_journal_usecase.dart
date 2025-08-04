3b55a84d74084db4358ef16d65eb21e87cc28669
USER <thakursusmi94@gmail.com>
2025-08-05 00:05:56 +0545

    added update journal usecase

--- lib/features/journaling/domain/use_case/update_journal_usecase.dart
+++ lib/features/journaling/domain/use_case/update_journal_usecase.dart
@@ -1,13 +1,13 @@
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
-// import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+import 'package:mindwave/features/journaling/domain/repository/journal_repository.dart';
 
 
-// class UpdateJournalUseCase {
-//   final JournalRepository repository;
+class UpdateJournalUseCase {
+  final JournalRepository repository;
 
-//   UpdateJournalUseCase(this.repository);
+  UpdateJournalUseCase(this.repository);
 
-//   Future<void> call(JournalEntity journal) async {
-//     await repository.updateJournal(journal);
-//   }
-// }
+  Future<void> call(JournalEntity journal) async {
+    await repository.updateJournal(journal);
+  }
+}
