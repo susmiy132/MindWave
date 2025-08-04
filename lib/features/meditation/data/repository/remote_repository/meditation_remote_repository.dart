37ddcb1cf806d8189702f7b915af0be6a41d061a
USER <thakursusmi94@gmail.com>
2025-08-05 00:20:20 +0545

    added meditation remote repository

--- lib/features/meditation/data/repository/remote_repository/meditation_remote_repository.dart
+++ lib/features/meditation/data/repository/remote_repository/meditation_remote_repository.dart
@@ -1,36 +1,36 @@
-// import 'package:mindwave/features/meditation/data/data_source/remote_datasource/meditation_remote_datasource.dart';
-// import 'package:mindwave/features/meditation/domain/entity/meditation_entity.dart';
-// import 'package:mindwave/features/meditation/domain/repository/meditation_repository.dart';
+import 'package:mindwave/features/meditation/data/data_source/remote_datasource/meditation_remote_datasource.dart';
+import 'package:mindwave/features/meditation/domain/entity/meditation_entity.dart';
+import 'package:mindwave/features/meditation/domain/repository/meditation_repository.dart';
 
-// class MeditationRepositoryImpl implements MeditationRepository {
-//   final MeditationRemoteDataSource remoteDataSource;
+class MeditationRepositoryImpl implements MeditationRepository {
+  final MeditationRemoteDataSource remoteDataSource;
 
-//   MeditationRepositoryImpl({required this.remoteDataSource});
+  MeditationRepositoryImpl({required this.remoteDataSource});
 
-//   @override
-//   Future<List<MeditationEntity>> getAllMeditations() async {
-//     try {
-//       return await remoteDataSource.getAllMeditations();
-//     } catch (e) {
-//       rethrow; // optionally handle/log error here
-//     }
-//   }
+  @override
+  Future<List<MeditationEntity>> getAllMeditations() async {
+    try {
+      return await remoteDataSource.getAllMeditations();
+    } catch (e) {
+      rethrow; // optionally handle/log error here
+    }
+  }
 
-//   @override
-//   Future<void> createMeditation(MeditationEntity meditation) async {
-//     try {
-//       await remoteDataSource.createMeditation(meditation);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
+  @override
+  Future<void> createMeditation(MeditationEntity meditation) async {
+    try {
+      await remoteDataSource.createMeditation(meditation);
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-//   @override
-//   Future<void> deleteMeditation(String id) async {
-//     try {
-//       await remoteDataSource.deleteMeditation(id);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-// }
+  @override
+  Future<void> deleteMeditation(String id) async {
+    try {
+      await remoteDataSource.deleteMeditation(id);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
