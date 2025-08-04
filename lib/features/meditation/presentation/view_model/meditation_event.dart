3bead0a0b894895b3e7384ed48e126e2bd040e0b
USER <thakursusmi94@gmail.com>
2025-08-05 00:23:28 +0545

    added meditation event

--- lib/features/meditation/presentation/view_model/meditation_event.dart
+++ lib/features/meditation/presentation/view_model/meditation_event.dart
@@ -1,31 +1,31 @@
-// // lib/features/meditation/presentation/view_model/meditation_event.dart
+// lib/features/meditation/presentation/view_model/meditation_event.dart
 
-// import 'package:equatable/equatable.dart';
-// import '../../domain/entity/meditation_entity.dart';
+import 'package:equatable/equatable.dart';
+import '../../domain/entity/meditation_entity.dart';
 
-// abstract class MeditationEvent extends Equatable {
-//   const MeditationEvent();
+abstract class MeditationEvent extends Equatable {
+  const MeditationEvent();
 
-//   @override
-//   List<Object?> get props => [];
-// }
+  @override
+  List<Object?> get props => [];
+}
 
-// class LoadMeditationsEvent extends MeditationEvent {}
+class LoadMeditationsEvent extends MeditationEvent {}
 
-// class AddMeditationEvent extends MeditationEvent {
-//   final MeditationEntity meditation;
+class AddMeditationEvent extends MeditationEvent {
+  final MeditationEntity meditation;
 
-//   const AddMeditationEvent(this.meditation);
+  const AddMeditationEvent(this.meditation);
 
-//   @override
-//   List<Object?> get props => [meditation];
-// }
+  @override
+  List<Object?> get props => [meditation];
+}
 
-// class DeleteMeditationEvent extends MeditationEvent {
-//   final String id;
+class DeleteMeditationEvent extends MeditationEvent {
+  final String id;
 
-//   const DeleteMeditationEvent(this.id);
+  const DeleteMeditationEvent(this.id);
 
-//   @override
-//   List<Object?> get props => [id];
-// }
+  @override
+  List<Object?> get props => [id];
+}
