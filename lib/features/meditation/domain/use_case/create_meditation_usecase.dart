1ceb523b5850244ef604786a961f571dd712f5a2
USER <thakursusmi94@gmail.com>
2025-08-05 00:21:11 +0545

    added create meditation usecase

--- lib/features/meditation/domain/use_case/create_meditation_usecase.dart
+++ lib/features/meditation/domain/use_case/create_meditation_usecase.dart
@@ -1,14 +1,14 @@
-// // lib/features/meditation/domain/usecase/create_meditation_usecase.dart
+// lib/features/meditation/domain/usecase/create_meditation_usecase.dart
 
-// import '../entity/meditation_entity.dart';
-// import '../repository/meditation_repository.dart';
+import '../entity/meditation_entity.dart';
+import '../repository/meditation_repository.dart';
 
-// class CreateMeditationUsecase {
-//   final MeditationRepository repository;
+class CreateMeditationUsecase {
+  final MeditationRepository repository;
 
-//   CreateMeditationUsecase(this.repository);
+  CreateMeditationUsecase(this.repository);
 
-//   Future<void> call(MeditationEntity meditation) {
-//     return repository.createMeditation(meditation);
-//   }
-// }
+  Future<void> call(MeditationEntity meditation) {
+    return repository.createMeditation(meditation);
+  }
+}
