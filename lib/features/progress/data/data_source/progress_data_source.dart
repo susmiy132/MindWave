6e5f36ad4a11b0b312b708cc282a623c13306f24
USER <thakursusmi94@gmail.com>
2025-08-05 00:35:49 +0545

    added data source

--- lib/features/progress/data/data_source/progress_data_source.dart
+++ lib/features/progress/data/data_source/progress_data_source.dart
@@ -1,8 +1,8 @@
-// import 'package:mindwave/features/progress/data/model/progress_hive_model.dart';
+import 'package:mindwave/features/progress/data/model/progress_hive_model.dart';
 
-// abstract class ProgressDataSource {
-//   Future<void> addProgress(ProgressHiveModel progress);
-//   Future<List<ProgressHiveModel>> getAllProgress();
-//   Future<void> updateProgress(ProgressHiveModel progress);
-//   Future<void> deleteProgress(String id);
-// }
+abstract class ProgressDataSource {
+  Future<void> addProgress(ProgressHiveModel progress);
+  Future<List<ProgressHiveModel>> getAllProgress();
+  Future<void> updateProgress(ProgressHiveModel progress);
+  Future<void> deleteProgress(String id);
+}
