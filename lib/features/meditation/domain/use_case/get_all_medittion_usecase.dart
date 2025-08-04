095c2ea5316660dc5fca16651fb8f2ad18de1cb0
USER <thakursusmi94@gmail.com>
2025-08-05 00:22:29 +0545

    added get all meditation usecase

--- lib/features/meditation/domain/use_case/get_all_medittion_usecase.dart
+++ lib/features/meditation/domain/use_case/get_all_medittion_usecase.dart
@@ -1,14 +1,14 @@
-// // lib/features/meditation/domain/usecase/get_all_meditation_usecase.dart
+// lib/features/meditation/domain/usecase/get_all_meditation_usecase.dart
 
-// import '../entity/meditation_entity.dart';
-// import '../repository/meditation_repository.dart';
+import '../entity/meditation_entity.dart';
+import '../repository/meditation_repository.dart';
 
-// class GetAllMeditationUsecase {
-//   final MeditationRepository repository;
+class GetAllMeditationUsecase {
+  final MeditationRepository repository;
 
-//   GetAllMeditationUsecase(this.repository);
+  GetAllMeditationUsecase(this.repository);
 
-//   Future<List<MeditationEntity>> call() {
-//     return repository.getAllMeditations();
-//   }
-// }
+  Future<List<MeditationEntity>> call() {
+    return repository.getAllMeditations();
+  }
+}
