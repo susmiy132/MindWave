95a3064f625b778328e88e9faf6d276ee9075e25
USER <thakursusmi94@gmail.com>
2025-08-04 23:57:48 +0545

    added journal repository

--- lib/features/journaling/domain/repository/journal_repository.dart
+++ lib/features/journaling/domain/repository/journal_repository.dart
@@ -1,12 +1,12 @@
-// import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
+import 'package:mindwave/features/journaling/domain/entity/journal_entity.dart';
 
 
-// abstract class JournalRepository {
-//   Future<void> addJournal(JournalEntity journal);
-//   Future<List<JournalEntity>> getJournals();
-//   Future<void> deleteJournal(String journalId);
-//   Future<void> updateJournal(JournalEntity journal);
+abstract class JournalRepository {
+  Future<void> addJournal(JournalEntity journal);
+  Future<List<JournalEntity>> getJournals();
+  Future<void> deleteJournal(String journalId);
+  Future<void> updateJournal(JournalEntity journal);
 
-//   Future<void> saveJournal(JournalEntity journal) async {}
-// }
+  Future<void> saveJournal(JournalEntity journal) async {}
+}
 
