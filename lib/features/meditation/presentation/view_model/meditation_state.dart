68a9e8ff5d91db365b7cac12c26436e32ede50d4
USER <thakursusmi94@gmail.com>
2025-08-05 00:23:56 +0545

    added meditation state

--- lib/features/meditation/presentation/view_model/meditation_state.dart
+++ lib/features/meditation/presentation/view_model/meditation_state.dart
@@ -1,33 +1,33 @@
-// // lib/features/meditation/presentation/view_model/meditation_state.dart
+// lib/features/meditation/presentation/view_model/meditation_state.dart
 
-// import 'package:equatable/equatable.dart';
-// import '../../domain/entity/meditation_entity.dart';
+import 'package:equatable/equatable.dart';
+import '../../domain/entity/meditation_entity.dart';
 
-// abstract class MeditationState extends Equatable {
-//   const MeditationState();
+abstract class MeditationState extends Equatable {
+  const MeditationState();
 
-//   @override
-//   List<Object?> get props => [];
-// }
+  @override
+  List<Object?> get props => [];
+}
 
-// class MeditationInitial extends MeditationState {}
+class MeditationInitial extends MeditationState {}
 
-// class MeditationLoading extends MeditationState {}
+class MeditationLoading extends MeditationState {}
 
-// class MeditationLoaded extends MeditationState {
-//   final List<MeditationEntity> meditations;
+class MeditationLoaded extends MeditationState {
+  final List<MeditationEntity> meditations;
 
-//   const MeditationLoaded(this.meditations);
+  const MeditationLoaded(this.meditations);
 
-//   @override
-//   List<Object?> get props => [meditations];
-// }
+  @override
+  List<Object?> get props => [meditations];
+}
 
-// class MeditationError extends MeditationState {
-//   final String message;
+class MeditationError extends MeditationState {
+  final String message;
 
-//   const MeditationError(this.message);
+  const MeditationError(this.message);
 
-//   @override
-//   List<Object?> get props => [message];
-// }
+  @override
+  List<Object?> get props => [message];
+}
