650d207d55dc290a77e6c64bf2af80a4fddba0ff
USER <thakursusmi94@gmail.com>
2025-08-05 00:15:24 +0545

    added meditation repository

--- lib/features/meditation/domain/repository/meditation_repository.dart
+++ lib/features/meditation/domain/repository/meditation_repository.dart
@@ -1,9 +1,9 @@
-// // lib/features/meditation/domain/repository/meditation_repository.dart
+// lib/features/meditation/domain/repository/meditation_repository.dart
 
-// import '../entity/meditation_entity.dart';
+import '../entity/meditation_entity.dart';
 
-// abstract class MeditationRepository {
-//   Future<List<MeditationEntity>> getAllMeditations();
-//   Future<void> createMeditation(MeditationEntity meditation);
-//   Future<void> deleteMeditation(String id);
-// }
+abstract class MeditationRepository {
+  Future<List<MeditationEntity>> getAllMeditations();
+  Future<void> createMeditation(MeditationEntity meditation);
+  Future<void> deleteMeditation(String id);
+}
